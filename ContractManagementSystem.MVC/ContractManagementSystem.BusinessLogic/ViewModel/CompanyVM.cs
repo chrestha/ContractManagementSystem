1b1981d79ac771eebfc4d9ef02288ec7e44ec67d
Anish Shrestha <anish.chrestha@hotmail.com>
2018-11-13 01:05:22 +1100

    View Models Added

---
+++ ContractManagementSystem.MVC/ContractManagementSystem.BusinessLogic/ViewModel/CompanyVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractManagementSystem.BusinessLogic.ViewModel
+{
+    public class CompanyVM
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string CompanyABN_CAN { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string URL { get; set; }
+        public  List<ContactVM> Tbl_Contact { get; set; }
+    }
+}
---
+++ ContractManagementSystem.MVC/ContractManagementSystem.BusinessLogic/ViewModel/ContactVM.cs
@@ -0,0 +1,16 @@
+﻿namespace ContractManagementSystem.BusinessLogic.ViewModel
+{
+    public class ContactVM
+    {
+        public int ID { get; set; }
+        public byte TitleId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool ContractType { get; set; }
+        public string Email { get; set; }
+        public string PhoneNo { get; set; }
+        public string Department { get; set; }
+        public int CompanyId { get; set; }
+        public CompanyVM company { get; set; }
+    }
+}
